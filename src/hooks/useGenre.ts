8904a1569c7ffbf96d577f2c644a8b79d0218f96
Mohd Izhak <hein_htet_23@hotmail.com>
2024-09-16 16:29:10 +0800

    Simply time calculations using ms library

--- src/hooks/useGenre.ts
+++ src/hooks/useGenre.ts
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import APIClient from "../services/api-client";
+import ms from "ms";
 import genre from "../data/genre";
 
 const apiClient = new APIClient<Genre>("/genres");
@@ -16,7 +17,7 @@ const useGenre = () =>
   useQuery({
     queryKey: ["genres"],
     queryFn: apiClient.getAll,
-    staleTime: 24 * 60 * 60 * 1000, //24h
+    staleTime: ms("24h"),
     initialData: genre,
   });
 
