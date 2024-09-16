f882ed94a991d499b46e9fda9d1433212c241628
Mohd Izhak <hein_htet_23@hotmail.com>
2024-09-16 12:44:58 +0800

    Refactor the genre hook to include react query

--- src/hooks/useGenre.ts
+++ src/hooks/useGenre.ts
@@ -1,4 +1,7 @@
+import { useQuery } from "@tanstack/react-query";
 import genre from "../data/genre";
+import useData, { FetchResponse } from "./useData";
+import apiClient from "../services/api-client";
 
 export interface Genre {
   id: number;
@@ -7,8 +10,17 @@ export interface Genre {
 }
 
 // adding static data to reduce API call as the genre does not change
-const useGenre = () => ({ data: genre, isLoading: false, error: null });
+const useGenre = () =>
+  useQuery({
+    queryKey: ["genres"],
+    queryFn: () =>
+      apiClient.get<FetchResponse<Genre>>("/genres").then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000, //24h
+    initialData: { count: genre.length, results: genre },
+  });
 
-//const useGenre = () => useData<Genre>("/genres");
+// Using React Query
+
+// const useGenre = () => useData<Genre>("/genres");
 
 export default useGenre;
